b2c28514763aa4d1a287ab28bb4e0e8d515133a3
smwoods <smwoods@bu.edu>
2016-01-09 14:51:36 -0500

    added 8.2

--- recursion_and_dynamic.py
+++ recursion_and_dynamic.py
@@ -13,7 +13,36 @@ def triple_step(n):
 			step_ct[i] += step_ct[i-1]
 	return step_ct[n-1]
 
+# 8.2 Robot in a Grid
+def robot_in_a_grid(grid):
+	rows = len(grid)
+	cols = len(grid[0])
+	visited = [[0 for _ in range(cols)] for _ in range(rows)]
+	visited[0][0] = 1
+	path = []
+	return get_path(grid, visited, rows-1, cols-1, path)
+
+def get_path(grid, visited, row, col, path):
+	if row < 0 or col < 0 or grid[row][col] == 0:
+		return False
+	else:
+		success = False
+		if row == 0 and col == 0:
+			path += [(row, col)]
+			success = True
+		elif get_path(grid, visited, row-1, col, path) or get_path(grid, visited, row, col-1, path):
+			path += [(row, col)]
+			success = True
+		visited[row][col] = 1
+		return success
+
 def main():
-	print(triple_step(5))
+	matrix = [
+		[1, 0, 1, 0, 1],
+		[1, 1, 1, 1, 1],
+		[1, 1, 0, 0, 1],
+		[1, 1, 0, 0, 1]
+	]
+	print(robot_in_a_grid(matrix))
 
 main()
